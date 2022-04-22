6f47e29f75bd59688a196897cd55f8fb6e4167f0
Mparker93 <marcuseparker@hotmail.com>
2022-04-22 01:13:50 +0100

    Creating visuals for each pieces legal moves. Adding logic to detect other pieces and boundaries for each move

--- chess.py
+++ chess.py
@@ -17,22 +17,26 @@ class Chess:
         self.setup = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
         self.add_pieces()
         self.set_board()
-        
+
         self.current_piece = None
         self.selected_piece = ()
         self.from_to_coordinates = []
-        
+
     def add_pieces(self) -> None:
         for i in range(8):
             self.white_pieces.append(Pawn(*coordinates_to_board(i, 6), white=True))
-            self.white_pieces.append(self.setup[i](*coordinates_to_board(i, 7), white=True))
+            self.white_pieces.append(
+                self.setup[i](*coordinates_to_board(i, 7), white=True)
+            )
             self.black_pieces.append(Pawn(*coordinates_to_board(i, 1), white=False))
-            self.black_pieces.append(self.setup[i](*coordinates_to_board(i, 0), white=False))
-    
+            self.black_pieces.append(
+                self.setup[i](*coordinates_to_board(i, 0), white=False)
+            )
+
     def set_board(self) -> None:
         for piece in self.white_pieces + self.black_pieces:
             self.add_piece(piece, board_to_coordinates(piece.x, piece.y))
-            
+
     def print(self) -> None:
         print(*self.board, sep="\n")
 
@@ -50,32 +54,64 @@ class Chess:
     def is_empty(self, coords: tuple[int, int]) -> bool:
         return self.board[coords[1]][coords[0]] == ""
 
-    def at_opposite_edge(self, coords: tuple[int, int], white: bool=True) -> bool:
+    def at_opposite_edge(self, coords: tuple[int, int], white: bool = True) -> bool:
         if white:
             return coords[1] == 7
         return coords[1] == 0
-    
+
     def update(self) -> None:
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             selected_piece = mouse_position(self.white_pieces + self.black_pieces)
             if isinstance(selected_piece, Piece):
                 self.current_piece = selected_piece
-                if self.selected_piece == board_to_coordinates(self.current_piece.x, self.current_piece.y):
+                self.current_piece.highlighted = True
+
+                if self.selected_piece == board_to_coordinates(
+                    self.current_piece.x, self.current_piece.y
+                ):
+                    self.current_piece.highlighted = False
                     self.selected_piece = ()
                     self.from_to_coordinates = []
                 else:
-                    self.selected_piece = board_to_coordinates(self.current_piece.x, self.current_piece.y)
+                    self.selected_piece = board_to_coordinates(
+                        self.current_piece.x, self.current_piece.y
+                    )
                     self.from_to_coordinates.append(self.selected_piece)
             else:
                 if len(self.from_to_coordinates) != 0:
-                    self.from_to_coordinates.append(board_to_coordinates(pyxel.mouse_x, pyxel.mouse_y))
-                    
+                    self.from_to_coordinates.append(
+                        board_to_coordinates(pyxel.mouse_x, pyxel.mouse_y)
+                    )
+
             if len(self.from_to_coordinates) == 2:
-                self.remove_piece(board_to_coordinates(self.current_piece.x, self.current_piece.y))
-                self.current_piece.x, self.current_piece.y = coordinates_to_board(self.from_to_coordinates[1][0], self.from_to_coordinates[1][1])  
-                self.add_piece(self.current_piece, board_to_coordinates(self.current_piece.x, self.current_piece.y))
-                print(notation(self.current_piece.symbol, board_to_coordinates(self.current_piece.x, self.current_piece.y)))
-                self.move_log.append(notation(self.current_piece.symbol, board_to_coordinates(self.current_piece.x, self.current_piece.y)))
+                self.remove_piece(
+                    board_to_coordinates(self.current_piece.x, self.current_piece.y)
+                )
+                self.current_piece.x, self.current_piece.y = coordinates_to_board(
+                    self.from_to_coordinates[1][0], self.from_to_coordinates[1][1]
+                )
+                self.add_piece(
+                    self.current_piece,
+                    board_to_coordinates(self.current_piece.x, self.current_piece.y),
+                )
+                if isinstance(self.current_piece, Pawn):
+                    self.current_piece.moved = True
+                print(
+                    notation(
+                        self.current_piece.symbol,
+                        board_to_coordinates(
+                            self.current_piece.x, self.current_piece.y
+                        ),
+                    )
+                )
+                self.move_log.append(
+                    notation(
+                        self.current_piece.symbol,
+                        board_to_coordinates(
+                            self.current_piece.x, self.current_piece.y
+                        ),
+                    )
+                )
                 self.selected_piece = ()
                 self.from_to_coordinates = []
 
@@ -84,4 +120,7 @@ class Chess:
         pyxel.bltm(x=0, y=0, tm=0, u=0, v=0, w=pyxel.width, h=pyxel.height)
         for pieces in self.white_pieces + self.black_pieces:
             pieces.draw()
-        pieces.draw_moves()
+
+        if self.current_piece:
+            if self.current_piece.highlighted:
+                self.current_piece.draw_moves()
--- pieces.py
+++ pieces.py
@@ -1,7 +1,15 @@
 import pyxel
 from piece import Piece
 from helpers import board_to_coordinates, coordinates_to_board
-from constants import GRIDSIZE, MOVE_SQUARE_OFFSET_X, MOVE_SQUARE_OFFSET_Y, WHITE_PIECES, BLACK_PIECES, DIRECTIONS
+from constants import (
+    GRIDSIZE,
+    MOVE_SQUARE_OFFSET_X,
+    MOVE_SQUARE_OFFSET_Y,
+    PIECE_HEIGHT,
+    WHITE_PIECES,
+    BLACK_PIECES,
+    DIRECTIONS,
+)
 
 
 class Pawn(Piece):
@@ -12,24 +20,29 @@ class Pawn(Piece):
         self.v = 0 if white else 16
         self.symbol = ""
         self.moves = DIRECTIONS["pawn"]
+        self.highlighted = False
         self.moved = False
         self.promote = False
 
     def move(self) -> None:
         pass
-    
+
     def possible_moves(self) -> list[tuple[int, int]]:
         move_coordinates = []
         for x, y in self.moves:
             if self.v == 0:
                 move_coordinates.append(
-                    (self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
+                    (
+                        self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
+                        self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y,
+                    )
                 )
             else:
                 move_coordinates.append(
-                    (self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
+                    (
+                        self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
+                        self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y,
+                    )
                 )
         return move_coordinates
 
@@ -53,9 +66,18 @@ class Pawn(Piece):
 
     def draw_moves(self) -> None:
         possible_moves = self.possible_moves()
-        for x, y in possible_moves:
-            pyxel.
-    
+        for x, y in possible_moves if not self.moved else possible_moves[:1]:
+            if self.v == 0:
+                if self.y > PIECE_HEIGHT:
+                    pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+                else:
+                    return None
+            else:
+                if self.y <= 7 * GRIDSIZE:
+                    pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+                else:
+                    return None
+
     def __str__(self) -> str:
         if self.v == 0:
             return f'{WHITE_PIECES["pawn"]}'
@@ -70,28 +92,34 @@ class Rook(Piece):
         self.v = 0 if white else 16
         self.symbol = "R"
         self.moves = DIRECTIONS["cardinal"]
+        self.highlighted = False
 
     def move(self) -> None:
         pass
-    
+
     def possible_moves(self) -> list[tuple[int, int]]:
         move_coordinates = []
         for x, y in self.moves:
-            if self.v == 0:
-                move_coordinates.append(
-                    (self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
-                )
-            else:
-                move_coordinates.append(
-                    (self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
-                )
+            for i in range(1, 8):
+                if self.v == 0:
+                    move_coordinates.append(
+                        (
+                            self.x - (x * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_X,
+                            self.y - (y * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_Y,
+                        )
+                    )
+                else:
+                    move_coordinates.append(
+                        (
+                            self.x + (x * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_X,
+                            self.y + (y * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_Y,
+                        )
+                    )
         return move_coordinates
 
     def get_coordinates(self) -> tuple[int, int]:
         return board_to_coordinates(self.x, self.y)
-    
+
     def draw(self) -> None:
         pyxel.blt(
             x=self.x,
@@ -103,7 +131,12 @@ class Rook(Piece):
             h=12,
             colkey=pyxel.COLOR_DARK_BLUE,
         )
-        
+
+    def draw_moves(self) -> None:
+        possible_moves = self.possible_moves()
+        for x, y in possible_moves:
+            pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+
     def __str__(self) -> str:
         if self.v == 0:
             return f'{WHITE_PIECES["rook"]}'
@@ -118,28 +151,34 @@ class Queen(Piece):
         self.v = 0 if white else 16
         self.symbol = "Q"
         self.moves = DIRECTIONS["cardinal"] + DIRECTIONS["diagonal"]
+        self.hightlighted = False
 
     def move(self) -> None:
         pass
-    
+
     def possible_moves(self) -> list[tuple[int, int]]:
         move_coordinates = []
         for x, y in self.moves:
-            if self.v == 0:
-                move_coordinates.append(
-                    (self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
-                )
-            else:
-                move_coordinates.append(
-                    (self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
-                )
+            for i in range(1, 8):
+                if self.v == 0:
+                    move_coordinates.append(
+                        (
+                            self.x - (x * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_X,
+                            self.y - (y * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_Y,
+                        )
+                    )
+                else:
+                    move_coordinates.append(
+                        (
+                            self.x + (x * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_X,
+                            self.y + (y * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_Y,
+                        )
+                    )
         return move_coordinates
 
     def get_coordinates(self) -> tuple[int, int]:
         return board_to_coordinates(self.x, self.y)
-    
+
     def draw(self) -> None:
         pyxel.blt(
             x=self.x,
@@ -151,7 +190,12 @@ class Queen(Piece):
             h=12,
             colkey=pyxel.COLOR_DARK_BLUE,
         )
-        
+
+    def draw_moves(self) -> None:
+        possible_moves = self.possible_moves()
+        for x, y in possible_moves:
+            pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+
     def __str__(self) -> str:
         if self.v == 0:
             return f'{WHITE_PIECES["queen"]}'
@@ -166,28 +210,33 @@ class King(Piece):
         self.v = 0 if white else 16
         self.symbol = "K"
         self.moves = DIRECTIONS["cardinal"] + DIRECTIONS["diagonal"]
+        self.highlighted = False
 
     def move(self) -> None:
         pass
-    
+
     def possible_moves(self) -> list[tuple[int, int]]:
         move_coordinates = []
         for x, y in self.moves:
             if self.v == 0:
                 move_coordinates.append(
-                    (self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
+                    (
+                        self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
+                        self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y,
+                    )
                 )
             else:
                 move_coordinates.append(
-                    (self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
+                    (
+                        self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
+                        self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y,
+                    )
                 )
         return move_coordinates
 
     def get_coordinates(self) -> tuple[int, int]:
         return board_to_coordinates(self.x, self.y)
-    
+
     def draw(self) -> None:
         pyxel.blt(
             x=self.x,
@@ -199,7 +248,12 @@ class King(Piece):
             h=12,
             colkey=pyxel.COLOR_DARK_BLUE,
         )
-        
+
+    def draw_moves(self) -> None:
+        possible_moves = self.possible_moves()
+        for x, y in possible_moves:
+            pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+
     def __str__(self) -> str:
         if self.v == 0:
             return f'{WHITE_PIECES["king"]}'
@@ -214,28 +268,33 @@ class Knight(Piece):
         self.v = 0 if white else 16
         self.symbol = "N"
         self.moves = DIRECTIONS["knight"]
+        self.highlighted = False
 
     def move(self) -> None:
         pass
-    
+
     def possible_moves(self) -> list[tuple[int, int]]:
         move_coordinates = []
         for x, y in self.moves:
             if self.v == 0:
                 move_coordinates.append(
-                    (self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
+                    (
+                        self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
+                        self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y,
+                    )
                 )
             else:
                 move_coordinates.append(
-                    (self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
+                    (
+                        self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
+                        self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y,
+                    )
                 )
         return move_coordinates
 
     def get_coordinates(self) -> tuple[int, int]:
         return board_to_coordinates(self.x, self.y)
-    
+
     def draw(self) -> None:
         pyxel.blt(
             x=self.x,
@@ -247,7 +306,12 @@ class Knight(Piece):
             h=12,
             colkey=pyxel.COLOR_DARK_BLUE,
         )
-        
+
+    def draw_moves(self) -> None:
+        possible_moves = self.possible_moves()
+        for x, y in possible_moves:
+            pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+
     def __str__(self) -> str:
         if self.v == 0:
             return f'{WHITE_PIECES["knight"]}'
@@ -262,28 +326,34 @@ class Bishop(Piece):
         self.v = 0 if white else 16
         self.symbol = "B"
         self.moves = DIRECTIONS["diagonal"]
+        self.highlighted = False
 
     def move(self) -> None:
         pass
-    
+
     def possible_moves(self) -> list[tuple[int, int]]:
         move_coordinates = []
         for x, y in self.moves:
-            if self.v == 0:
-                move_coordinates.append(
-                    (self.x - (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y - (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
-                )
-            else:
-                move_coordinates.append(
-                    (self.x + (x * GRIDSIZE) + MOVE_SQUARE_OFFSET_X,
-                     self.y + (y * GRIDSIZE) + MOVE_SQUARE_OFFSET_Y)
-                )
+            for i in range(1, 8):
+                if self.v == 0:
+                    move_coordinates.append(
+                        (
+                            self.x - (x * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_X,
+                            self.y - (y * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_Y,
+                        )
+                    )
+                else:
+                    move_coordinates.append(
+                        (
+                            self.x + (x * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_X,
+                            self.y + (y * GRIDSIZE * i) + MOVE_SQUARE_OFFSET_Y,
+                        )
+                    )
         return move_coordinates
 
     def get_coordinates(self) -> tuple[int, int]:
         return board_to_coordinates(self.x, self.y)
-    
+
     def draw(self) -> None:
         pyxel.blt(
             x=self.x,
@@ -295,7 +365,12 @@ class Bishop(Piece):
             h=12,
             colkey=pyxel.COLOR_DARK_BLUE,
         )
-        
+
+    def draw_moves(self) -> None:
+        possible_moves = self.possible_moves()
+        for x, y in possible_moves:
+            pyxel.rect(x=x, y=y, w=6, h=6, col=pyxel.COLOR_RED)
+
     def __str__(self) -> str:
         if self.v == 0:
             return f'{WHITE_PIECES["bishop"]}'
